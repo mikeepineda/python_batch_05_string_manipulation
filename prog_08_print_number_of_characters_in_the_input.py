7f3bccb67c19785b88ac8985b8c098d1d31a6582
incessmikee <incess.mikee@gmail.com>
2025-03-19 21:45:18 +0800

    prog 08: code complete

--- prog_08_print_number_of_characters_in_the_input.py
+++ prog_08_print_number_of_characters_in_the_input.py
@@ -1,5 +1,10 @@
 # Prog08: Create a program that ask the user to input their fullname. Print the number of characters in the input.
 
 # ask for user's input
+fullname = input("Enter your fullname: ")
+
 # count total characters used including spaces
+character_counter = len(fullname)
+
 # print
+print(character_counter)
