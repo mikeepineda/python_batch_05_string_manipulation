79b6d63cdf93cb502e0053fa42b279c3124dbe09
incessmikee <incess.mikee@gmail.com>
2025-03-19 21:03:17 +0800

    prog 03: code complete

--- prog_03_print_input_capital_number.py
+++ prog_03_print_input_capital_number.py
@@ -1,5 +1,7 @@
 # Prog03: Create a program that ask the user to input their fullname. Print the input in all capital letter.
 
 # ask user's input
-# convert input to all uppercase letters.
-# print input
+fullname = input ("Enter your full name: ")
+
+# convert input to all uppercase letters and print input
+print(fullname.upper())
