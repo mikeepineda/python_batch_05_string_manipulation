c409636c670333a1728d4ef3f95ec1609463af09
incessmikee <incess.mikee@gmail.com>
2025-03-19 22:00:14 +0800

    prog 10: pseudocode

---
+++ prog_10_print_input_in_snake_case.py
@@ -0,0 +1,6 @@
+# Prog10: Create a program that ask the user to input their fullname in incorrect casing. Print the input in snake case.
+
+# ask for user's input
+# convert all input into lowercase
+# change space to underscore
+# print 
