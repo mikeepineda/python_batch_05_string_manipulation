8989bff9fdedee5a2fbd62a12d4eaf0ed4b36ce5
incessmikee <incess.mikee@gmail.com>
2025-03-19 21:00:33 +0800

    prog 03: psuedocode

---
+++ prog_03_print_input_capital_number.py
@@ -0,0 +1,5 @@
+# Prog03: Create a program that ask the user to input their fullname. Print the input in all capital letter.
+
+# ask user's input
+# convert input to all uppercase letters.
+# print input
