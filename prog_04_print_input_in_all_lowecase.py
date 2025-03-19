60b4838060ad7d34eed4a79f2dce7cd69b3b9ddc
incessmikee <incess.mikee@gmail.com>
2025-03-19 21:06:39 +0800

    prog 04: pseudocode

---
+++ prog_04_print_input_in_all_lowecase.py
@@ -0,0 +1,5 @@
+# Prog04: Create a program that ask the user to input their fullname. Print the input in all lower case.
+
+# ask user's input
+# convert user's input into lowercase
+# print user's input
