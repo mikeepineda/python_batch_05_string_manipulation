67dafa890d62b21bcd76925510ac8ba2001412ed
incessmikee <incess.mikee@gmail.com>
2025-03-19 22:03:16 +0800

    prog 10: ask user's input, convert, print

--- prog_10_print_input_in_snake_case.py
+++ prog_10_print_input_in_snake_case.py
@@ -1,6 +1,11 @@
 # Prog10: Create a program that ask the user to input their fullname in incorrect casing. Print the input in snake case.
 
 # ask for user's input
+fullname = input("Enter your fullname in incorrect casing: ")
+
 # convert all input into lowercase
+snake_case = fullname.lower()
+
 # change space to underscore
 # print 
+print(snake_case)
