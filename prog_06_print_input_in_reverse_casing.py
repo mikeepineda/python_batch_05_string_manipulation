3b13857e961cc10dd62537e75d1be263e01f6527
incessmikee <incess.mikee@gmail.com>
2025-03-19 21:24:34 +0800

    prog 06: code complete

--- prog_06_print_input_in_reverse_casing.py
+++ prog_06_print_input_in_reverse_casing.py
@@ -1,5 +1,7 @@
 # Prog06: Create a program that ask the user to input their fullname in incorrect casing. Print each character of the input in reverse casing.
 
 # ask for user's input
-# reverse the uppercase to lowercase and vice versa
-# print
+fullname = input("Enter your full name: ")
+
+# reverse the uppercase to lowercase and vice versa and print
+print(fullname.swapcase())
