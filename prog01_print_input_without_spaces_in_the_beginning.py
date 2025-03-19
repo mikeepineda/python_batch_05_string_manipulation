c3e8b179e36122563a6b8f7191ef05fdfb7abfb6
incessmikee <incess.mikee@gmail.com>
2025-03-19 19:55:44 +0800

    prog 01: psuedocode

---
+++ prog01_print_input_without_spaces_in_the_beginning.py
@@ -0,0 +1,5 @@
+# Prog01: Create a program that ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning.
+
+# ask for user's input
+# remove leading spaces from the input
+# print input
