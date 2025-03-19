679bb5eadd35039a2f01f7d2af1b4bdb32e52db8
incessmikee <incess.mikee@gmail.com>
2025-03-19 21:35:42 +0800

    prog 07: ask user's input, split

--- prog_07_print_number_of_input_words.py
+++ prog_07_print_number_of_input_words.py
@@ -1,6 +1,10 @@
 # Prog07: Create a program that ask the user to input a complete statement. Print the number of words in the input.
 
 # ask for user's input
+statement = input("Enter a statement: ")
+
 # splits the statement into words
+word_count = statement.split()
+
 # counts the number of words in the list
 # print
