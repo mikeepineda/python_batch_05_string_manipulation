c62673470f77f7ac6272676aed3686e4d5ca93b5
incessmikee <incess.mikee@gmail.com>
2025-03-19 21:33:21 +0800

    prog 07: pseudocode

---
+++ prog_07_print_number_of_input_words.py
@@ -0,0 +1,6 @@
+# Prog07: Create a program that ask the user to input a complete statement. Print the number of words in the input.
+
+# ask for user's input
+# splits the statement into words
+# counts the number of words in the list
+# print
