c182e2e560d49e6661fab72e15be9218584f128a
incessmikee <incess.mikee@gmail.com>
2025-03-19 21:42:12 +0800

    prog 08: pseudocode

---
+++ prog_08_print_number_of_characters_in_the_input.py
@@ -0,0 +1,5 @@
+# Prog08: Create a program that ask the user to input their fullname. Print the number of characters in the input.
+
+# ask for user's input
+# count total characters used including spaces
+# print
