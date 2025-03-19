6c385b6dc29138b699c279672d90cce51e775ce2
incessmikee <incess.mikee@gmail.com>
2025-03-19 20:20:00 +0800

    prog 02: code complete

--- prog_02_print_number_in_six_digit_number.py
+++ prog_02_print_number_in_six_digit_number.py
@@ -1,5 +1,10 @@
 # Prog02: Create a program that ask the user to input a number (0-1000). Print the number in 6 digit format. Add zeros at the beginning to complete the 6 digit.
 
 # ask for user's input
+number = int(input("Enter a number from 0-1000: "))
+
 # formats the number as a 6-digit string
+formatted_number = f"{number:06d}"
+
 # print input
+print(formatted_number)
