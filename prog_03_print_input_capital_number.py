1d63edf08714c368884166aa1e060e6e104d9ed1
incessmikee <incess.mikee@gmail.com>
2025-03-19 21:14:07 +0800

    prog 03: remove space, code complete

--- prog_03_print_input_capital_number.py
+++ prog_03_print_input_capital_number.py
@@ -1,7 +1,7 @@
 # Prog03: Create a program that ask the user to input their fullname. Print the input in all capital letter.
 
 # ask user's input
-fullname = input ("Enter your full name: ")
+fullname = input("Enter your full name: ")
 
 # convert input to all uppercase letters and print input
 print(fullname.upper())
