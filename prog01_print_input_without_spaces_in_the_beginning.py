384154b068a7ac259e3c15ce73e80d5ee6ed1957
incessmikee <incess.mikee@gmail.com>
2025-03-19 20:03:58 +0800

    remove first draft

--- prog01_print_input_without_spaces_in_the_beginning.py
+++
@@ -1,9 +0,0 @@
-# Prog01: Create a program that ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning.
-
-# ask for user's input
-
-fullname = input("Enter your full name: ").lstrip() # remove leading spaces from the input
-
-# print input
-
-print(fullname)
