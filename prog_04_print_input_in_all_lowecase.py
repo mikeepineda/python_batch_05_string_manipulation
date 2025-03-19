6a911ed1b9ce787fdec21cfac952ce0b9ed88c79
incessmikee <incess.mikee@gmail.com>
2025-03-19 21:11:49 +0800

    prog 04: code complete

--- prog_04_print_input_in_all_lowecase.py
+++ prog_04_print_input_in_all_lowecase.py
@@ -1,5 +1,7 @@
 # Prog04: Create a program that ask the user to input their fullname. Print the input in all lower case.
 
 # ask user's input
-# convert user's input into lowercase
-# print user's input
+fullname = input("Enter your full name: ")
+
+# convert user's input into lowercase and print user's input
+print(fullname.lower())
