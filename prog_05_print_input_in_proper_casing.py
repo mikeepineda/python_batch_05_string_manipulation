0ff2e377a3a8ed4a1f048ad06d99d47c3e694f36
incessmikee <incess.mikee@gmail.com>
2025-03-19 21:17:06 +0800

    prog 05: pseudocode

---
+++ prog_05_print_input_in_proper_casing.py
@@ -0,0 +1,5 @@
+# Prog05: Create a program that ask the user to input their fullname in incorrect casing. Print the input in proper casing.
+
+# ask for user's input
+# convert into proper casing
+# print 
