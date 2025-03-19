62d63ed788c1c81d033ddf0d82032ae12a98ee3b
incessmikee <incess.mikee@gmail.com>
2025-03-19 21:22:14 +0800

    prog 06: pseudocode

---
+++ prog_06_print_input_in_reverse_casing.py
@@ -0,0 +1,5 @@
+# Prog06: Create a program that ask the user to input their fullname in incorrect casing. Print each character of the input in reverse casing.
+
+# ask for user's input
+# reverse the uppercase to lowercase and vice versa
+# print
