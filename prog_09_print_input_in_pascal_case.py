76a91a27e0b491da3dc1a15e5c1d26c7eb7496bf
incessmikee <incess.mikee@gmail.com>
2025-03-19 21:52:36 +0800

    prog 09: ask user's input, capitalize first letter

--- prog_09_print_input_in_pascal_case.py
+++ prog_09_print_input_in_pascal_case.py
@@ -1,6 +1,10 @@
 # Prog09: Create a program that ask the user to input their fullname in incorrect casing. Print the input in pascal case.
 
 # ask for user's input 
+fullname = input("Enter your name in incorrect casing: ")
+
 # capitalize letter of every first word
+pascal_case = fullname.title()
+
 # remove spacing 
 # print
