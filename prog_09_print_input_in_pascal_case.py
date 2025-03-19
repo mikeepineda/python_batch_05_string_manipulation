1e2d805de8ba0c8f2f2248f2207f91486ec899e8
incessmikee <incess.mikee@gmail.com>
2025-03-19 21:48:30 +0800

    prog 09: pseudocode

---
+++ prog_09_print_input_in_pascal_case.py
@@ -0,0 +1,6 @@
+# Prog09: Create a program that ask the user to input their fullname in incorrect casing. Print the input in pascal case.
+
+# ask for user's input 
+# capitalize letter of every first word
+# remove spacing 
+# print
